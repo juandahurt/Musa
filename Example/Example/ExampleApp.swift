bb546966219c5045651ecb2d4ac68d2cf7965b33
juandahurt <juandahurtadom@gmail.com>
2026-06-19 19:21:57 -0500

    basic setup

---
+++ Example/Example/ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleApp.swift
+//  Example
+//
+//  Created by apolo on 19/06/26.
+//
+
+import SwiftUI
+
+@main
+struct ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
--- Musa/Package.swift
+++ Musa/Package.swift
@@ -4,24 +4,20 @@
 import PackageDescription
 
 let package = Package(
-    name: "Musa",
+    name: "MyLibrary",
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
-            name: "Musa",
-            targets: ["Musa"]
+            name: "MyLibrary",
+            targets: ["MyLibrary"]
         ),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "Musa",
-            swiftSettings: [
-                .enableUpcomingFeature("ApproachableConcurrency"),
-            ],
+            name: "MyLibrary"
         ),
 
-    ],
-    swiftLanguageModes: [.v6]
+    ]
 )
